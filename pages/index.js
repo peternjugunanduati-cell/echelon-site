63d710e41a3c1b5c31002d89353eaa0ceb61b607
peternjugunanduati-cell <peternjugunanduati@gmail.com>
2026-01-26 15:59:36 +0300

    Minimal working version for deployment

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", padding: "2rem" }}>
+      <h1>Echelon Interiors</h1>
+      <p>Luxury Interior Design & Fit-Out Company in Nairobi</p>
+      <p>Portfolio coming soon...</p>
+    </div>
+  );
+}
